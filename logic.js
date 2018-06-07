453f1f14844ec3fefbd6a59f12df8898868bb8da
caleb <caleb.sears22@gmail.com>
2018-06-07 14:16:00 -0400

    small update

--- logic.js
+++ logic.js
@@ -1,21 +1,3 @@
-const inquirer = require('inquirer')
-const db = require('mysql')
-
-const connection = db.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'Bamazon'
-})
-
-connection.connect()
-
-connection.query('SELECT * FROM `products`', (err, data)=>{
-    if(err){
-        console.log(err)
-    } 
-    for (const product of data){
-        console.log(product.price)
-    }
-    connection.end()
-})
+const { prompt } = require('inquirer')
+const { initial_questions } = require('./assets/js/questions')
+require('console.table')
